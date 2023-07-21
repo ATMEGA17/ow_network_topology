2a409254f19a51e9d3006c2a9d105c800b352769
Youcef <atmega17@gmail.cil>
2023-07-21 19:42:44 +0100

    INIT INFRA - RADIUS GO

--- setup.py
+++ setup.py
@@ -40,5 +40,10 @@ if sys.argv[-1] == 'publish':
 
 
 setup(
+    platforms=['Platform Independent'],
+    keywords=['django', 'netjson', 'openwrt', 'networking', 'openwisp'],
+    packages=find_packages(exclude=['tests*', 'docs*']),
+    include_package_data=True,
+    zip_safe=False,
     install_requires=get_install_requires(),
 )
